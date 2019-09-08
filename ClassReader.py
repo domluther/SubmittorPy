4dd1d07f0658cbbb62134cdea9772d7144559102
domluther <35171942+domluther@users.noreply.github.com>
2019-09-08 13:35:29 +0100

    Made case insensitive

--- ClassReader.py
+++ ClassReader.py
@@ -3,12 +3,16 @@ from time import sleep
 
 
 class Class:
+    """Stores information about an individual class"""
 
     def __init__(self, name):
         self.class_name = name
         self.class_path = ''
         self.students = []
         self.reset()
+        self.number_of_students = self.count_students()
+        self.work_found = 0
+        self.all_found = False
 
     def set_path(self, path):
         self.class_path = Path(path)
@@ -17,34 +21,58 @@ class Class:
         self.students.append(Student(student_name))
 
     def add_unrecognised_file(self, file_name):
+        """Keeps track of files that aren't associated with a student"""
         if file_name not in self.unrecognised_files:
             self.unrecognised_files.append(file_name)
 
     def reset_unrecognised_files(self):
+        """Clears the list of unrecognised files. Used to stop multiple copies of the same file being in there.
+        Could alternately make it check for existence, but this seems faster."""
         self.unrecognised_files = []
 
     def reset(self):
+        """Resets read in file information. Stops a file being found, then vanishing and still showing as found."""
         self.reset_unrecognised_files()
         for student in self.students:
             student.reset_file_name()
+        self.work_found = 0
+
+    def count_found(self):
+        for student in self.students:
+            if student.file_found:
+                self.work_found += 1
+
+    def count_students(self):
+        self.number_of_students = len(self.students)
+
+    def check_if_finished(self):
+        self.count_students()
+        self.count_found()
+        print(f'Found {self.work_found}/{self.number_of_students}')
+        if self.work_found == self.number_of_students:
+            self.all_found = True
 
     def __repr__(self):
         return f'Class {self.class_name}'
 
 
 class Student:
+    """Stores information about individual students"""
 
     def __init__(self, name):
         self.student_name = name
-        self.found = False
+        self.file_found = False
         self.reset_file_name()
 
     def set_file_name(self, file_name):
+        # Marks the student as found.
         self.file_name = f'<p id="found">{file_name}<p>'
-        self.found = True
+        self.file_found = True
 
     def reset_file_name(self):
+        # Default value is missing.
         self.file_name = '<p id="error">Missing<p>'
+        self.file_found = False
 
     def __repr__(self):
         repr = f'Student: {self.student_name}, file: {self.file_name}'
@@ -52,116 +80,148 @@ class Student:
 
 
 def read_classes():
+    """Reads in class details from the associated file"""
     classes = []
+    class_list_file = 'class_list.txt'
 
-    with open('class_list.txt') as f:
+    with open(class_list_file) as f:
+        # Set default values
         lines = f.readlines()
         class_name_set = False
         class_path_set = False
-        students = []
+        # Keeps track of which class we are up to
         class_index = 0
-        for line in lines:
 
-            line = line.strip()
-            if line.startswith('Class') and not class_name_set:
-                try:
-                    class_name = line.replace('Class ', '')
-                    classes.append(Class(class_name))
-                    class_name_set = True
-                except IndexError:
-                    print(f'Class name not found: {line}.')
-                    break
-            elif line.startswith('Path') and not class_path_set:
-                try:
-                    class_path = line.replace('Path ', '')
-                    classes[class_index].set_path(class_path)
-                    class_path_set = True
-                except IndexError:
-                    print(f'Path found not specified: {line}.')
-                    break
+        for line in lines:
+            # Iterate through the lines of the file
+            line = line.strip()  # To remove \n
+
+            if line.startswith('Class'):
+                if not class_name_set:
+                    # A class can only have one name
+                    try:
+                        class_name = line.replace('Class ', '')
+                        classes.append(Class(class_name))
+                        class_name_set = True
+                    except IndexError:
+                        print(f'Class name not found: {line}.')
+                        break
+                else:
+                    # If there is more than one Class line before an ENDCLASS is found.
+                    raise ValueError('A class can not have multiple names. Previous class not terminated properly?')
+            elif line.startswith('Path'):
+                # Class name must be set first, and only one path can be set for a class.
+                if class_name_set:
+                    if not class_path_set:
+                        try:
+                            class_path = line.replace('Path ', '')
+                            classes[class_index].set_path(class_path)
+                            class_path_set = True
+                        except IndexError:
+                            print(f'Path found not specified: {line}.')
+                            break
+                    else:
+                        # If there is more than one Path line before an ENDCLASS is found
+                        raise ValueError('A class can not have multiple paths. Previous class not terminated properly?')
+                else:
+                    raise ValueError('Class name must be set before path is set.')
             elif line.startswith('END'):
                 print(f'End of class {class_name} found.')
                 class_index += 1
-
                 class_name_set = False
                 class_path_set = False
             else:
-                student_name = line
-                classes[class_index].add_student(student_name)
+                if class_name_set and class_path_set:
+                    # Only doable if the class has a name and path
+                    student_name = line
+                    classes[class_index].add_student(student_name)
+                else:
+                    raise ValueError('Class name and path must be set before students are added')
     return classes
 
 
-def create_html(individual_class):
-    html = f'''<html><head>
-    <title> {individual_class.class_name}</title>
-    <meta http-equiv="refresh" content="10">
-    <style>
-    #error {{
-      color: red;
-    }}
-    #found {{
-      color: green;
-    }}
-    </style>
-    
+def generate_html(individual_class):
+    html = f'''<html>
 
+    <head>
+        <title> Class {individual_class.class_name}</title>
+            <meta http-equiv="refresh" content="10">
+            <style>
+                #error {{
+                  color: red;
+                }}
+                #found {{
+                  color: green;
+                }}
+            </style>    
     </head>
+
     <body>
-    <h1>Submittor for {individual_class.class_name}</h1>
-    <h2>Submit to {individual_class.class_path}</h2>
-    <table>
-    <tr>
-    <th>Student name</th>
-    <th>Submission status</th>
-      </tr>    '''
+        <h1>Submittor for {individual_class.class_name}</h1>
+        <h2>Submit to {individual_class.class_path}</h2>
+
+        <h3>{individual_class.work_found}/{individual_class.number_of_students} submitted</h3>
+        <table>
+            <tr>
+                <th>Student name</th>
+                <th>Submission status</th>
+            </tr>'''
 
     for student in individual_class.students:
-        html += f'''<tr>
-        <th>{student.student_name}</th>
-        <th>{student.file_name}</th>
-      </tr>'''
+        html += f'''
+        <tr>
+            <th>{student.student_name}</th>
+            <th>{student.file_name}</th>
+        </tr>'''
 
-    html += '''</table>'''
+    html += '</table>'
 
     if individual_class.unrecognised_files:
+        # If there are unrecognised files, list them.
         html += '''
         <h2>Unrecognised files</h2>
         <ul>'''
 
-        for file in individual_class.unrecognised_files:
-            html += f'''<li>{file}</li>'''
-        html += '''</ul>'''
+        for unrecognised_file in individual_class.unrecognised_files:
+            html += f'''<li id="error">{unrecognised_file}</li>'''
+        html += '</ul>'
 
     with open('page.html', 'w') as f:
+        # Turn that into HTML
         f.write(html)
 
+
 def check_for_files(individual_class):
-    # Reset data each time it is checked. Marginaly more inefficient but fixes issue of data that was there vanishing and being shown as there.
+    """Checks the path to find all folders."""
     individual_class.reset()
-    # Read in all files in the path
+
     if individual_class.class_path.exists():
+        # Read in all files in the path
         list_of_files = individual_class.class_path.glob('*.*')
         files = [x for x in list_of_files if x.is_file()]
-        # Create a dict of statuses for each student
+
+        # Nested loop through files + students to add information about student.
         for file in files:
             file_name = file.parts[-1]
-            print(file)
             for student in individual_class.students:
                 student_name = student.student_name
-                if student_name in file_name:
-                    size = file.stat().st_size
-                    if size == 0:
-                        print(f'Found {file_name} but empty')
-                        student.file_name = '<p id="error">Empty file - copy again<p>'
-                    else:
-                        print(f'Found {student_name}')
+                if student_name.lower() in file_name.lower():
+                    if file.stat().st_size > 0:
+                        print(f'Found "{file_name}" by {student_name}')
                         student.set_file_name(file_name)
+                    else:
+                        # If the file is empty
+                        print(f'Found {file_name} but it is empty')
+                        student.file_name = '<p id="error">Empty file - copy again<p>'
                     break
             else:
+                print(f'No owner found for {file}')
                 individual_class.add_unrecognised_file(file)
+        individual_class.check_if_finished()
+        print()
     else:
-        print('Path does not exist')
-    a=123
+        raise ValueError(f'{individual_class.class_path} does not exist')
+
 
 def show_menu(classes):
     print('Please choose a class:')
@@ -179,13 +239,14 @@ def main():
     # Present the menu so user chooses class
     chosen_class = show_menu(classes)
 
-    while True:
+    while not chosen_class.all_found:
         # Check the folder for files
         check_for_files(chosen_class)
 
         # Generate an output
-        create_html(chosen_class)
+        generate_html(chosen_class)
 
         sleep(5)
+
 
 main()
